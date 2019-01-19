34fcd338c7a438860bf10463a83d2c83bd59595d
Markus Rubey <markus@rubey.net>
2019-01-19 16:09:37 +0100

    Add destinations for Favorites, Search and Details

---
+++ presentation/src/main/java/net/rubey/yoyocinema/search/SearchFragment.kt
@@ -0,0 +1,37 @@
+package net.rubey.yoyocinema.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_favorites.*
+import net.rubey.yoyocinema.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_searchFragment_to_detailsFragment)
+        }
+    }
+}
